6a78a12d13742340d89c7bf56b56c78d12efa5ae
ema <mccoy39082@163.com>
2023-01-19 19:12:13 +0800

    Humanized abyss hint

---
+++ src/GenshinSwitch/Views/Converters/DateTimeToDayStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace GenshinSwitch.Views.Converters;
+
+internal class DateTimeToDayStringConverter : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is DateTimeOffset time)
+        {
+            return time.ToString("yyyy.MM.dd");
+        }
+        if (value is DateTime time1)
+        {
+            return time1.ToString("yyyy.MM.dd");
+        }
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/GenshinSwitch/Views/Converters/DateTimeToTimeStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace GenshinSwitch.Views.Converters;
+
+internal class DateTimeToTimeStringConverter : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is DateTimeOffset time)
+        {
+            return time.ToString("HH:mm:ss.fff");
+        }
+        if (value is DateTime time1)
+        {
+            return time1.ToString("HH:mm:ss.fff");
+        }
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
